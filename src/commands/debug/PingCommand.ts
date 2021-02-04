0c59979affd85577d3bf0156245c48e1f65a1a1c
Taso <taso@projecterror.dev>
2021-02-04 13:02:07 -0500

    refactor(loggers): Refactored to inheritance based loggers

--- src/commands/debug/PingCommand.ts
+++ src/commands/debug/PingCommand.ts
@@ -1,10 +1,12 @@
 import { Message, MessageEmbed } from 'discord.js';
-import { Command } from 'discord-akairo';
+import { Command, CommandHandler } from 'discord-akairo';
 import dayjs from 'dayjs';
 import { discordCodeBlock } from '../../utils/miscUtils';
+import { Logger } from 'tslog';
 
 export default class PingCommand extends Command {
-  constructor() {
+  private readonly _logger: Logger;
+  constructor(handler: CommandHandler) {
     super('ping', {
       aliases: ['ping'],
       description: {
@@ -20,32 +22,42 @@ export default class PingCommand extends Command {
       ],
       channel: 'guild',
     });
+
+    this._logger = handler.client.log.getChildLogger({
+      name: 'PingCmd',
+      prefix: ['[PingCmd]'],
+    });
   }
 
   public async exec(msg: Message): Promise<Message | undefined> {
-    const pingEmbed = new MessageEmbed()
-      .setColor('#ff8f00')
-      .setAuthor('Bot Latency Info')
-      .setTimestamp()
-      .setFooter(msg.author.tag, msg.author.displayAvatarURL());
+    try {
+      const pingEmbed = new MessageEmbed()
+        .setColor('#ff8f00')
+        .setAuthor('Bot Latency Info')
+        .setTimestamp()
+        .setFooter(msg.author.tag, msg.author.displayAvatarURL());
 
-    const sentMsg = await msg.util?.send(pingEmbed);
+      const sentMsg = await msg.util?.send(pingEmbed);
 
-    const diffTime = dayjs(sentMsg?.createdAt).diff(msg.createdAt, 'ms');
+      const diffTime = dayjs(sentMsg?.createdAt).diff(msg.createdAt, 'ms');
 
-    pingEmbed.addFields([
-      {
-        inline: true,
-        name: 'RTT:',
-        value: discordCodeBlock(diffTime + 'ms'),
-      },
-      {
-        inline: true,
-        name: 'HeartBeat:',
-        value: discordCodeBlock(Math.round(this.client.ws.ping) + 'ms'),
-      },
-    ]);
+      pingEmbed.addFields([
+        {
+          inline: true,
+          name: 'RTT:',
+          value: discordCodeBlock(diffTime + 'ms'),
+        },
+        {
+          inline: true,
+          name: 'HeartBeat:',
+          value: discordCodeBlock(Math.round(this.client.ws.ping) + 'ms'),
+        },
+      ]);
 
-    return msg.util?.send(pingEmbed);
+      return msg.util?.send(pingEmbed);
+    } catch (e) {
+      this._logger.error(e);
+      return msg.channel.send('');
+    }
   }
 }
--- src/listeners/commandHandler/CommandFiredListener.ts
+++ src/listeners/commandHandler/CommandFiredListener.ts
@@ -1,17 +1,22 @@
-import { Listener, Command } from 'discord-akairo';
+import { Listener, Command, ListenerHandler } from 'discord-akairo';
 import { Message } from 'discord.js';
+import { Logger } from 'tslog';
 
 export default class CommandListener extends Listener {
-  public constructor() {
+  private readonly _logger: Logger;
+
+  public constructor(handler: ListenerHandler) {
     super('commandStarted', {
       event: 'commandStarted',
       emitter: 'commandHandler',
     });
+
+    this._logger = handler.client.log.getChildLogger({
+      name: 'CmdFiredListener',
+      prefix: ['[CmdFiredListener]'],
+    });
   }
   public exec(msg: Message, cmd: Command): void {
-    const logger = this.client.log.getChildLogger({
-      name: 'CmdFired',
-    });
-    logger.info(`Command: \`${cmd}\` triggered by ${msg.author.tag}. RAW: ${msg}`);
+    this._logger.info(`Command: \`${cmd}\` triggered by ${msg.author.tag}. RAW: ${msg}`);
   }
 }
