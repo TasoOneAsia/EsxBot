89281bcd07c5205b7b8ab975cf9d3910990d1cd5
Poggu <poggudev@gmail.com>
2021-02-04 20:01:52 +0100

    refactor(cleanStack): From 70 lines to 1 pepeLaugh

--- src/utils/cleanStack.ts
+++ src/utils/cleanStack.ts
@@ -1,9 +1,9 @@
-import os from 'os';
+import path from 'path';
 import escapeStringRegexp from 'escape-string-regexp';
 
-const extractPathRegex = /\s+at.*[(\s](.*)\)?/;
+const extractPathRegex = /\s+at.*[(\s](.*)\)?/g;
 const pathRegex = /^(?:(?:(?:node|(?:(?:node:)?internal\/[\w/]*|.*node_modules\/(?:babel-polyfill|pirates)\/.*)?\w+)(?:\.js)?:\d+:\d+)|native)/;
-const homeDir = typeof os.homedir === 'undefined' ? '' : os.homedir();
+const homeDir = process.cwd().replace(/\\/g, '\\\\');
 
 export interface ICleanStackOpts {
   /**
@@ -26,35 +26,6 @@ export const cleanStack = (
   stack: string,
   { pretty, basePath }: ICleanStackOpts = {}
 ): string => {
-  const basePathRegex =
-    basePath && new RegExp(`(at | \\()${escapeStringRegexp(basePath)}`, 'g');
-
-  return stack
-    .replace(/\\/g, '/')
-    .split('\n')
-    .filter((line) => {
-      const pathMatches = line.match(extractPathRegex);
-      if (pathMatches === null || !pathMatches[1]) {
-        return true;
-      }
-
-      const match = pathMatches[1];
-
-      return !pathRegex.test(match);
-    })
-    .filter((line) => line.trim() !== '')
-    .map((line) => {
-      if (basePathRegex) {
-        line = line.replace(basePathRegex, '$1');
-      }
-
-      if (pretty) {
-        line = line.replace(extractPathRegex, (m, p1) =>
-          m.replace(p1, p1.replace(homeDir, '~'))
-        );
-      }
-
-      return line;
-    })
-    .join('\n');
+  //const basePathRegex = basePath && new RegExp(`(at | \\()${escapeStringRegexp(basePath)}`, 'g');
+  return stack.replace(new RegExp(homeDir, 'g'), '~').replace(/\\/g, '/');
 };
