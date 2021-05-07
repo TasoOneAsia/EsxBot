ba18464ab2bee837fcd2e59df83daa48c361cf80
Taso <taso@projecterror.dev>
2021-05-07 16:04:43 -0400

    feat(commands/exec): Debug exec permissions for host machine
    This is inherently dangerous aka reason I only get perms for it :)

--- src/client/EsxBot.ts
+++ src/client/EsxBot.ts
@@ -24,6 +24,7 @@ declare module 'discord-akairo' {
     db: Connection;
     commandHandler: CommandHandler;
     listenerHandler: ListenerHandler;
+    managerHandler: ManagerHandler;
     _actions: Actions;
   }
 }
---
+++ src/commands/debug/ExecCommand.ts
@@ -0,0 +1,47 @@
+import { Command, CommandHandler } from 'discord-akairo';
+import { Message, Util } from 'discord.js';
+import { Logger } from 'tslog';
+import { exec, ChildProcess } from 'child_process';
+
+export default class ExecCommand extends Command {
+  private log: Logger;
+
+  public constructor(handler: CommandHandler) {
+    super('exec', {
+      aliases: ['exec'],
+      description: {
+        content: 'Runs shell commands for debug and the meme',
+        usage: 'exec <command> [...args]',
+        examples: ['exec pwd', 'exec curl https://tasoagc.dev'],
+      },
+      category: 'Debug',
+      ownerOnly: true,
+      args: [
+        {
+          id: 'input',
+          match: 'rest',
+          prompt: {
+            start: (message: Message) =>
+              `${message.author}, what commands do you want to exec?`,
+          },
+        },
+      ],
+    });
+
+    this.log = handler.client.log.getChildLogger({
+      name: 'ExecCmd',
+      prefix: ['[ExecCmd]'],
+    });
+  }
+
+  public async exec(msg: Message, { input }: { input: string }): Promise<ChildProcess> {
+    return exec(input, async (error, stdOut) => {
+      let output = (error || stdOut) as string | string[];
+      // Split int
+      output = Util.splitMessage(`\`\`\`javascript\n${output}\`\`\``);
+      for (const line of output) {
+        await msg.channel.send(line);
+      }
+    });
+  }
+}
