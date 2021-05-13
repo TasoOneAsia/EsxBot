b01cc5bae4b23f8ae8594436b0f3b0b10ee4b9de
Taso <taso@projecterror.dev>
2021-05-12 23:38:10 -0400

    tweak(providers/typeorm): Improve event handling slightly

--- src/structures/TypeORMProvider.ts
+++ src/structures/TypeORMProvider.ts
@@ -2,14 +2,23 @@ import { Provider } from 'discord-akairo';
 import GuildSettings from '../models/GuildSettings';
 import { DeleteResult, InsertResult, Repository } from 'typeorm';
 import { GuildSettingsJSON } from '../types';
-import { defaultSettingMap } from '../utils/constants';
+import { defaultGuildSettings } from '../utils/constants';
 import { Logger } from 'tslog';
 import EventEmitter from 'events';
+import { noop } from '../utils';
+
+export enum TypeOrmProviderEvents {
+  SettingChanged = 'settingChanged',
+  AllSettingsChanged = 'allSettingsChanged',
+  SettingDeleted = 'settingDeleted',
+  ConfigCleared = 'configCleared',
+  SettingsInit = 'settingsInit',
+}
 
 export default class TypeORMProvider extends Provider {
   public ['constructor']: typeof TypeORMProvider;
   private readonly log: Logger;
-  public settingsEmitter = new EventEmitter();
+  public readonly eventEmitter = new EventEmitter();
 
   public constructor(private readonly repo: Repository<GuildSettings>, log: Logger) {
     super();
@@ -24,6 +33,8 @@ export default class TypeORMProvider extends Provider {
     for (const setting of settings) {
       this.items.set(setting.config_set, setting.settings);
     }
+
+    this.eventEmitter.emit(TypeOrmProviderEvents.SettingsInit);
   }
 
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
@@ -36,22 +47,25 @@ export default class TypeORMProvider extends Provider {
     this.log.silly(`Key (${key}) requested in ${configKey}`);
     if (this.items.has(configKey)) {
       const value = this.items.get(configKey)[key];
-      return value ?? defaultSettingMap[key];
+      return value ?? defaultGuildSettings[key];
     }
 
-    return defaultSettingMap[key];
+    return defaultGuildSettings[key];
   }
 
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
   public async delete(key: string, configIndex = 0): Promise<InsertResult> {
     const configKey = this.constructor.getItemsPrefix(configIndex);
-    const data = this.items.get(configKey) || {};
     this.log.debug(`Key: (${key}) was deleted in ${configKey}`);
+
+    const data = this.items.get(configKey) || {};
+    // Copy object
+    const oldValue = { ...data }[key];
 
     delete data[key];
 
-    return this.repo
+    const insertResult = await this.repo
       .createQueryBuilder()
       .insert()
       .into(GuildSettings)
@@ -59,12 +73,23 @@ export default class TypeORMProvider extends Provider {
       .onConflict('("config_set") DO UPDATE SET "settings" =:settings')
       .setParameter('settings', null)
       .execute();
+
+    this.eventEmitter.emit(TypeOrmProviderEvents.SettingDeleted, key, oldValue);
+
+    return insertResult;
   }
 
   public async setAll(value: GuildSettingsJSON, configIndex = 0): Promise<InsertResult> {
     const configKey = this.constructor.getItemsPrefix(configIndex);
-    this.log.debug(`${configKey} was set to`, value);
-    return this.repo
+    this.log.debug(`Key: '${configKey}' was set to`, value);
+
+    const oldData = await this.repo.findOne({
+      where: {
+        config_set: configKey,
+      },
+    });
+
+    const insertResult = await this.repo
       .createQueryBuilder()
       .insert()
       .into(GuildSettings)
@@ -72,6 +97,10 @@ export default class TypeORMProvider extends Provider {
       .onConflict('("config_set") DO UPDATE SET "settings" = :settings')
       .setParameter('settings', value)
       .execute();
+
+    this.eventEmitter.emit(TypeOrmProviderEvents.AllSettingsChanged, oldData || null);
+
+    return insertResult;
   }
 
   public async set(
@@ -80,15 +109,18 @@ export default class TypeORMProvider extends Provider {
     configIndex = 0
   ): Promise<InsertResult> {
     const configKey = this.constructor.getItemsPrefix(configIndex);
-    this.log.debug(`Key was set for ${configKey}`, newValue);
+
+    this.log.debug(`Key '${key}' in ConfigSet '${configKey} was set to:' `, newValue);
+
     const data = this.items.get(configKey) || {};
+    // Lets copy to a new object so we can pass ref with no issue
     const oldValue = { ...data }[key];
+
+    // Update our values
     data[key] = newValue;
     this.items.set(configKey, data);
 
-    this.settingsEmitter.emit('settingChanged', key, oldValue);
-
-    return this.repo
+    const insertResults = await this.repo
       .createQueryBuilder()
       .insert()
       .into(GuildSettings)
@@ -96,28 +128,42 @@ export default class TypeORMProvider extends Provider {
       .onConflict('("config_set") DO UPDATE SET "settings" = :settings')
       .setParameter('settings', data)
       .execute();
+
+    this.eventEmitter.emit(TypeOrmProviderEvents.SettingChanged, key, oldValue);
+
+    return insertResults;
   }
 
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
   public async clear(configIndex = 0): Promise<DeleteResult> {
     const configKey = this.constructor.getItemsPrefix(configIndex);
-    this.log.debug(`Config: ${configKey} was cleared`);
+    this.log.debug(`Config: ${configKey} was totally cleared`);
     this.items.delete(configKey);
 
-    this.settingsEmitter.emit('settingsChanged');
+    const oldData = await this.repo.find({
+      where: {
+        config_set: configKey,
+      },
+    });
+
+    // We only emit the event if the ConfigSet actually existed
+    oldData ? this.eventEmitter.emit(TypeOrmProviderEvents.ConfigCleared) : noop();
 
     return this.repo.delete(configKey);
   }
 
   public getAllValues(configIndex = 0): GuildSettingsJSON {
     const configKey = this.constructor.getItemsPrefix(configIndex);
-    this.log.silly(`The value of ${configKey} was requested`);
     const data = this.items.get(configKey) || {};
 
-    this.settingsEmitter.emit('settingsChanged');
+    this.log.silly(
+      `All values of ConfigSet '${configKey}' were requested, value returned:`,
+      data
+    );
 
-    return { ...defaultSettingMap, ...data };
+    // We spread the defaultGuildSettings which then get overwritten by fetched data
+    return { ...defaultGuildSettings, ...data };
   }
 
   private static getItemsPrefix(index: number): string {
